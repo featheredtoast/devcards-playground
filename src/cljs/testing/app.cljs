516cee5c2b81b49fb46af489749ff00ae7741d70
Jeff Wong <awole20@gmail.com>
2016-03-30 13:24:05 -0700

    splitting application space out of display-to-html
    better use cases for devcards

--- src/cljs/testing/app.cljs
+++ src/cljs/testing/app.cljs
@@ -1,17 +1,20 @@
-(ns testing.core
+(ns testing.app
   (:require [reagent.core :as reagent :refer [atom]]
-            [testing.app]
             [devcards.core])
   (:require-macros
      ;; Notice that I am not including the 'devcards.core namespace
      ;; but only the macros. This helps ensure that devcards will only
      ;; be created when the :devcards is set to true in the build config.
      [devcards.core :as dc :refer [defcard defcard-rg defcard-doc]]))
-
-(enable-console-print!)
-
+(defonce app-state (atom {:text "Hello Chestnut!"}))
 (defn greeting []
-  [:h1 (:text @testing.cards/app-state)])
+  [:h1 (:text @app-state)])
 
-(defn main [] (reagent/render [greeting] (js/document.getElementById "app")))
-(main)
+(defcard-doc
+  "this is a documentation thing")
+
+(defcard "a card")
+(defcard reagent-no-help
+  (reagent/as-element [:h1 "Reagent example"]))
+(defcard-rg main-page
+  (greeting))
